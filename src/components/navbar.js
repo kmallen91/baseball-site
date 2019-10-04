85c7f4e63dce4853adc9f5a5326a6c106b727a8e
Kevin Allen <kevin.allen91@gmail.com>
2019-10-03 18:26:28 -0700

    added axios, started navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Navbar = () => {
+
+    return (
+        <div className="navbar">
+            <nav className="nav">
+                <a href="">Home <a href="">
+                <a href="">Teams <a href="">
+                <a href="">Players <a href="">
+
+            </nav>
+            <form className="search">
+            Search
+            <input type="text" name="search" placeholder="Search" /> 
+            </form>
+        </div>
+    )
+
+}
