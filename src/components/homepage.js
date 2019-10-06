7eae7370d31a7b1e8a0a986e7940877bc8cd1399
Kevin Allen <kevin.allen91@gmail.com>
2019-10-05 21:30:33 -0700

    api working, team component started

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,31 +1,20 @@
 import React, {useEffect} from "react"
 import useInput from "./useInput.js"
-import teamCard from "./teamCard.js"
+import TeamCard from "./teamCard.js"
+import axios from "axios"
 
 const Homepage = () => {
-const [teams, setTeams] = useInput([])
+
 const [players, setPlayers] = useInput([])
 
-useEffect (()=> {
-    fetch("https://api.sportsdata.io/v3/mlb/scores/json/TeamSeasonStats/2019", {
-	"method": "GET",
-	"headers": 	{	
-		"Ocp-Apim-Subscription-Key": "8b6b2979daf04317a5c067a795998b5f"
-        }
-    })
-    .then(response => {
-	    console.log(response);
-        // setTeams(response)
-    })
-    .catch(err => {
-	    console.log(err);
-    });
-})
+
 
 
 return (
-    <div>
-    TEST
+    <div >
+    
+        <TeamCard />
+    
     </div>
 
 
--- src/components/teamCard.js
+++ src/components/teamCard.js
@@ -1,16 +1,39 @@
-import React from "react"
+import React, {useEffect} from "react"
+import useInput from "./useInput.js"
+import axios from "axios"
 
-const teamCard = (props) => {
+const TeamCard = () => {
+    const [teams, setTeams] = useInput([])
+
+    useEffect (()=> {
+    axios.get("https://api.sportsdata.io/v3/mlb/scores/json/AllTeams", {
+	"method": "GET",
+	"headers": 	{	
+		"Ocp-Apim-Subscription-Key": "8b6b2979daf04317a5c067a795998b5f"
+        }
+    })
+    .then(response => {
+	    console.log(response.data);
+        setTeams(response.data)
+    })
+    .catch(err => {
+	    console.log(err);
+    });
+    }, [])
 
     return (
-        <div className="card">
-            <h2 className="team-name"> {props.mlb_org} </h2>
-            <h3 className="team-venue"> {props.venue_name} </h3>
-            <p className="team_division"> {props.division_full} </p>
+        <div>
+        {teams.map(team => (
+            <div className="card" key={team.Key}>
+                <h2 className="team-name"> {team.Name} </h2>
+                <h3 className="team-location"> {team.City} </h3>
+                <p className="team_division"> {team.League} {team.Division} </p>
 
+            </div>
+        ))}
         </div>
 
     )
 }
 
-export default teamCard
+export default TeamCard
