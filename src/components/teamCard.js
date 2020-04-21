a454be360dce51683dc28ee4ce8aef331b00dd49
Kevin Allen <kevin.allen91@gmail.com>
2020-04-21 12:46:34 -0700

    fixed team page map, css

--- src/components/teamCard.js
+++ src/components/teamCard.js
@@ -1,42 +1,18 @@
 import React, { useEffect } from "react";
-import useInput from "./useInput.js";
-import axios from "axios";
-import { Link } from "react-router-dom";
-import IndividualTeam from "./IndividualTeam";
 
-const TeamCard = () => {
-  const [teams, setTeams] = useInput([]);
 
-  useEffect(() => {
-    axios
-      .get("https://api.sportsdata.io/v3/mlb/scores/json/teams", {
-        method: "GET",
-        headers: {
-          "Ocp-Apim-Subscription-Key": "8b6b2979daf04317a5c067a795998b5f"
-        }
-      })
-      .then(response => {
-        console.log(response.data);
-        setTeams(response.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }, [setTeams]);
-
+const TeamCard = (props) => {
+  const { key, name, city, league, division } = props
   return (
-    <div className="card-container">
-      {teams.map(team => (
-        // <Link to={IndividualTeam} key={team.Key}>
-        <div className="card" key={team.Key}>
-          <h2 className="team-name"> {team.Name} </h2>
-          <h3 className="team-location"> {team.City} </h3>
+    <div >      
+        <div className="card" >
+          <h2 className="team-name"> {name} </h2>
+          <h3 className="team-location"> {city} </h3>
           <p className="team_division">
-            {team.League} {team.Division}
+            {league} {division}
           </p>
         </div>
-        // </Link>
-      ))}
+      
     </div>
   );
 };
--- src/components/teamPage.js
+++ src/components/teamPage.js
@@ -1,18 +1,41 @@
-import React from "react"
+import React, {useEffect} from "react"
+import useInput from "./useInput.js";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import TeamCard from './teamCard.js'
 
 
 const TeamPage = () => {
 
-return (
-    <div >
-    
-        <TeamCard />
+    const [teams, setTeams] = useInput([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.sportsdata.io/v3/mlb/scores/json/teams", {
+        method: "GET",
+        headers: {
+          "Ocp-Apim-Subscription-Key": "8b6b2979daf04317a5c067a795998b5f"
+        }
+      })
+      .then(response => {
+        console.log(response.data);
+        setTeams(response.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [setTeams]);
+
+  return (
+    <div className="card-container" >
+        {teams.map(team => (
+            <Link to={`/teams/${team.Name}`} >
+            <TeamCard key={team.Key} name={team.Name} city={team.City} league={team.League} division={team.Division}/>
+            </Link>
+        ))}
     
     </div>
-
-
-)
+  )
 
 }
 
