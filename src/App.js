67ccde5f35fc6bef7f6ff0b752b42ad7c12cd203
Kevin Allen <kevin.allen91@gmail.com>
2020-04-23 13:03:40 -0700

    fixed styling on team and player cards

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Route } from "react-router-dom";
+
 //Components
 import Navbar from "./components/navbar.js";
 import Homepage from "./components/homepage.js";
 import TeamPage from "./components/teamPage.js";
 import PlayerPage from './components/playerPage';
 import AllPlayersPage from './components/allPlayersPage'
-
+import IndividualTeam from "./components/IndividualTeam.js";
 
 //Styling
 import "./styles/navbar.css";
 import "./styles/teamCard.css";
 import "./App.css";
 import "./styles/homepage.css";
-import IndividualTeam from "./components/IndividualTeam.js";
 
 function App() {
   return (
--- src/components/teamCard.js
+++ src/components/teamCard.js
@@ -7,9 +7,9 @@ const TeamCard = (props) => {
   return (
     <div >      
         <div className="team-card" >
-          <h2 className="team-name"> {name} </h2>
-          <h3 className="team-location"> {city} </h3>
-          <p className="team_division">
+          <h2 className="team-name team-info"> {name} </h2>
+          <h3 className="team-location team-info"> {city} </h3>
+          <p className="team_division team-info">
             {league} {division}
           </p>
           <img className='team-logo' src={logo} alt=''/>
