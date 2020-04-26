394ede9317c908b712fd576a98a036d31a358027
Kevin Allen <kevin.allen91@gmail.com>
2020-04-25 17:20:00 -0700

    updated player card to differ for pitchers/hitters, updated to show message for non active players

--- src/components/playerPage.jsx
+++ src/components/playerPage.jsx
@@ -51,9 +51,9 @@ const PlayerPage = () => {
     }, []);
 
   
-  if (player && stats){
-    const { FirstName, LastName, BatHand, ThrowHand, Height, Weight, BirthDate, College, PhotoUrl, Experience, Position } = player
-    const { Started, Games, AtBats, Runs, Hits, Singles, Doubles, Triples, HomeRuns, RunsBattedIn, BattingAverage, Strikeouts, Walks, StolenBases } = stats
+  if (player && stats && player.Status == 'Active'){
+    const {Status, FirstName, LastName, BatHand, ThrowHand, Height, Weight, BirthDate, College, PhotoUrl, Experience, Position } = player
+    const {PositionCategory, Started, Games, AtBats, Runs, Hits, Singles, Doubles, Triples, HomeRuns, RunsBattedIn, BattingAverage, Strikeouts, Walks, StolenBases, PitchesThrown, PitchingBattingAverageAgainst, PitchingBlownSaves, PitchingCompleteGames, PitchingDoubles, PitchingEarnedRuns, PitchingFlyOuts, PitchingHitByPitch, PitchingHits, PitchingNoHitters, PitchingOnBasePercentage, PitchingOnBasePlusSlugging, PitchingPerfectGames, PitchingPlateAppearances, PitchingQualityStarts, PitchingRuns, PitchingShutOuts, PitchingSingles, PitchingStrikeouts, PitchingStrikeoutsPerNineInnings, PitchingTotalBases, PitchingTriples, PitchingWalks, PitchingWalksPerNineInnings, Saves, PitchingHomeRuns } = stats
 
     function formatDate(date) {
       let d = new Date(date),
@@ -76,50 +76,115 @@ const PlayerPage = () => {
     }
   
     const formattedBirthDay = formatDate(BirthDate)
-      const formattedHeight = formatHeight(Height)
+    const formattedHeight = formatHeight(Height)
+
+    if (PositionCategory == 'P') {
+      return (
+        <div className='player-container'>
+            <img className='player-page-photo player-info' src={PhotoUrl} alt=''/>
+            <div className='player-info-container'>
+              <h2 className="player-name player-info"> {FirstName} {LastName} </h2>
+              <h3 className="player-bats-hits player-info"> Bats: {BatHand} Throws: {ThrowHand} </h3>
+              <p className="player-age">
+                Position: {Position}
+              </p>
+              <p className="player-height-weight player-info">
+                Height: {formattedHeight} Weight: {Weight}lbs.
+              </p>
+              <p className="player-info">
+                Birthdate: {formattedBirthDay} 
+              </p>
+              <p className="player-info">
+                College: {College ? College : 'N/A'} 
+              </p>
+              <p className="player-info">
+                Years in League: {Experience}
+              </p>
+            </div>
+            
+            <div className='player-stat-container'>
+              <p className="player-info"> Pitches Thrown: {PitchesThrown} </p>
+              <p className="player-info">  Pitching Batting Average Against: { PitchingBattingAverageAgainst} </p>
+              <p className="player-info"> Blown Saves: {PitchingBlownSaves}</p>
+              <p className="player-info"> Complete Games: {PitchingCompleteGames}</p>              
+              <p className="player-info"> Perfect Games: {PitchingPerfectGames}</p>              
+              <p className="player-info"> Pitching Singles: {PitchingSingles}</p>
+              <p className="player-info"> Pitching Doubles: {PitchingDoubles}</p>
+              <p className="player-info"> Pitching Triples: {PitchingTriples}</p>
+              <p className="player-info"> Pitching Home Runs: {PitchingHomeRuns}</p>
+              <p className="player-info"> Earned Runs: {PitchingEarnedRuns}</p>
+              <p className="player-info"> Pitching Fly Outs: {PitchingFlyOuts}</p>
+              <p className="player-info"> Hit By Pitch: {PitchingHitByPitch}</p>
+              <p className="player-info"> Hits: {PitchingHits}</p>
+              <p className="player-info"> No Hitters: {PitchingNoHitters}</p>
+              <p className="player-info"> Batting Average: {BattingAverage}</p>
+              <p className="player-info"> Pitching On Base Percentage: {PitchingOnBasePercentage}</p>
+              <p className="player-info"> Pitching On Base Plus Slugging: {PitchingOnBasePlusSlugging}</p>
+              <p className="player-info"> Pitching Plate Appearances: {PitchingPlateAppearances}</p>
+              <p className="player-info"> Quality Starts: {PitchingQualityStarts}</p>
+              <p className="player-info"> Runs: {PitchingRuns}</p>
+              <p className="player-info"> Shut Outs: {PitchingShutOuts}</p>              
+              <p className="player-info"> Strikeouts: {PitchingStrikeouts}</p>
+              <p className="player-info"> Strikeouts Per Nine Innings: {PitchingStrikeoutsPerNineInnings}</p>
+              <p className="player-info"> Total Bases: {PitchingTotalBases}</p>              
+              <p className="player-info"> Walks: {PitchingWalks}</p>
+              <p className="player-info">  Walks Per Nine Innings: {PitchingWalksPerNineInnings}</p>
+              <p className="player-info"> Saves: {Saves}</p>
+            </div>
+        </div>
+    )} else {
+
+      return (
+        <div className='player-container'>
+            <img className='player-page-photo player-info' src={PhotoUrl} alt=''/>
+            <div className='player-info-container'>
+              <h2 className="player-name player-info"> {FirstName} {LastName} </h2>
+              <h3 className="player-bats-hits player-info"> Bats: {BatHand} Throws: {ThrowHand} </h3>
+              <p className="player-age">
+                Position: {Position}
+              </p>
+              <p className="player-height-weight player-info">
+                Height: {formattedHeight} Weight: {Weight}lbs.
+              </p>
+              <p className="player-info">
+                Birthdate: {formattedBirthDay} 
+              </p>
+              <p className="player-info">
+                College: {College ? College : 'N/A'} 
+              </p>
+              <p className="player-info">
+                Years in League: {Experience}
+              </p>
+            </div>
+            <div className='player-stat-container'>
+              <p className="player-info"> Games Started: {Started} </p>
+              <p className="player-info"> Games Played: {Games} </p>
+              <p className="player-info"> At Bats: {AtBats}</p>
+              <p className="player-info"> Runs: {Runs}</p>
+              <p className="player-info"> Hits: {Hits}</p>
+              <p className="player-info"> Singles: {Singles}</p>
+              <p className="player-info"> Doubles: {Doubles}</p>
+              <p className="player-info"> Triples: {Triples}</p>
+              <p className="player-info"> Home Runs: {HomeRuns}</p>
+              <p className="player-info"> RBI: {RunsBattedIn}</p>
+              <p className="player-info"> Batting Average: {BattingAverage}</p>
+              <p className="player-info"> Strikeouts: {Strikeouts}</p>
+              <p className="player-info"> Walks: {Walks}</p>
+              <p className="player-info"> Stolen Bases: {StolenBases}</p>
+            </div>
+        </div>
+    )}
+  }   
+  else if (player && player.Status != 'Active'){
     return (
-      <div className='player-container'>
-          <img className='player-page-photo player-info' src={PhotoUrl} alt=''/>
-          <div className='player-info-container'>
-            <h2 className="player-name player-info"> {FirstName} {LastName} </h2>
-            <h3 className="player-bats-hits player-info"> Bats: {BatHand} Throws: {ThrowHand} </h3>
-            <p className="player-age">
-              Position: {Position}
-            </p>
-            <p className="player-height-weight player-info">
-              Height: {formattedHeight} Weight: {Weight}lbs.
-            </p>
-            <p className="player-info">
-              Birthdate: {formattedBirthDay} 
-            </p>
-            <p className="player-info">
-              College: {College ? College : 'N/A'} 
-            </p>
-            <p className="player-info">
-              Years in League: {Experience}
-            </p>
-          </div>
-          <div className='player-stat-container'>
-            <p className="player-info"> Games Started: {Started} </p>
-            <p className="player-info"> Games Played: {Games} </p>
-            <p className="player-info"> At Bats: {AtBats}</p>
-            <p className="player-info"> Runs: {Runs}</p>
-            <p className="player-info"> Hits: {Hits}</p>
-            <p className="player-info"> Singles: {Singles}</p>
-            <p className="player-info"> Doubles: {Doubles}</p>
-            <p className="player-info"> Triples: {Triples}</p>
-            <p className="player-info"> Home Runs: {HomeRuns}</p>
-            <p className="player-info"> RBI: {RunsBattedIn}</p>
-            <p className="player-info"> Batting Average: {BattingAverage}</p>
-            <p className="player-info"> Strikeouts: {Strikeouts}</p>
-            <p className="player-info"> Walks: {Walks}</p>
-            <p className="player-info"> Stolen Bases: {StolenBases}</p>
-          </div>
+      <div className='not-active'>
+        Player Not on Active Roster
       </div>
-  )}
+    )
+  }
   else {
     return (
-      <div>
+      <div className='loading'>
         Loading...
       </div>
     )
