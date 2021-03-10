7b4b009fa85a25dd37adff971c15c1e8ba4f386c
Kevin Allen <kevin.allen91@gmail.com>
2021-03-09 19:38:20 -0800

    changing homepage links to buttons

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -7,8 +7,8 @@ const Homepage = () => {
 return (
     <div className="page-container" >    
         <h2>MLB STATS PAGE </h2>
-        <Link to ='/teams'>Visit the Teams here!</Link><br/> <br/>    
-        <Link to ='/players'>Find all the Players here!</Link>    
+        <Link to ='/teams' className='team-link'>Visit the Teams here!</Link><br/> <br/>    
+        <Link to ='/players' className='player-link'>Find all the Players here!</Link>    
     </div>
 )}
 
